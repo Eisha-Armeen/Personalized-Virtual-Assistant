5df79f95049bd5c29458f48fbba67caa59cd817b
Eisha Armeen <armeeneisha@gmail.com>
2024-08-23 11:00:59 +0500

    Add files via upload

---
+++ java/com/example/majorapp/ui/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.majorapp.ui;
+
+import androidx.annotation.NonNull;
+
+import com.example.majorapp.data.Message;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class MainActivity {
+
+
+  
+
+
+}
